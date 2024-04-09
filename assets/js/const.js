60273e192eb014e303cc67ab6fc71275143cbd10
Huy Quang <huyq706@gmail.com>
2024-04-09 18:44:06 +0700

    delete submenu

--- assets/js/const.js
+++ assets/js/const.js
@@ -20,7 +20,7 @@ const URL_CLIENT_LOCAL = "http://127.0.0.1:5500";
 // const URL_HOSTING_LOCAL = "https://do-an3-fe.vercel.app";
 const URL_HOSTING_LOCAL = URL_CLIENT_LOCAL;
 // URL Production
-const URL_SERVER_LOCAL = "https://localhost:5001";
+const URL_SERVER_LOCAL = "https://api.tipslife.site";
 
 //Product
 const PAGE_NUMBER_DEFAULT = 1;
